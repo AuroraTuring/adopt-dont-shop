aa14da64c87df73f2acc9864b4841c3daa09c6c4
Noah Durbin <13364668+noahdurbin@users.noreply.github.com>
2024-05-17 17:59:11 -0600

    feat: worked on applications

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -1,19 +1,24 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.describe "applications show page" do
-  # As a visitor
-  # When I visit an applications show page
-  # Then I can see their attributes
-  describe "as a visitor" do
-    describe "when I visit an applications show page" do
-      it "then I can see their attributes" do
-        visit "/:id"
-
-        expect(page).to have_content(application.name)
-        expect(page).to have_content(application.address)
-        expect(page).to have_content(application.description)
-        expect(page).to have_content(application.all_pets)
-        expect(page).to have_content(application.status)
+RSpec.describe 'applications show page' do
+  before(:each) do
+    @shelter1 = Shelter.create!(foster_program: true, name: 'Whimsy Whiskers', city: 'Denver', rank: 42)
+    # @pet1 = Pet.create!(name: 'Buddy', breed: 'Golden Retriever', age: 3, adoptable: true, shelter: @shelter1)
+    @pet1 = @shelter1.pets.create!(name: 'Buddy', breed: 'Golden Retriever', age: 3, adoptable: true)
+    @application1 = Application.create!(name: 'John Doe', address: '123 Elm St',
+                                        description: 'Looking for a friendly dog', status: 'Pending')
+    PetApplication.create!(pet: @pet1, application: @application1)
+  end
+  describe 'as a visitor' do
+    describe 'when I visit an applications show page' do
+      it 'then I can see their attributes' do
+        visit '/applications/1'
+        save_and_open_page
+        expect(page).to have_content(@application1.name)
+        expect(page).to have_content(@application1.address)
+        expect(page).to have_content(@application1.description)
+        expect(page).to have_content(@application1.all_pets.name)
+        expect(page).to have_content(@application1.status)
       end
     end
   end
