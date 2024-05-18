015200d7ad3bb4ecdc47976fb359d62ce32c8727
Jan Lehigh <janlehigh461437@gmail.com>
2024-05-17 18:30:52 -0600

    Add refactored test in applications/show_spec.rb. Complete User Story One

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -5,19 +5,21 @@ RSpec.describe 'applications show page' do
     @shelter1 = Shelter.create!(foster_program: true, name: 'Whimsy Whiskers', city: 'Denver', rank: 42)
     # @pet1 = Pet.create!(name: 'Buddy', breed: 'Golden Retriever', age: 3, adoptable: true, shelter: @shelter1)
     @pet1 = @shelter1.pets.create!(name: 'Buddy', breed: 'Golden Retriever', age: 3, adoptable: true)
-    @application1 = Application.create!(name: 'John Doe', address: '123 Elm St',
-                                        description: 'Looking for a friendly dog', status: 'Pending')
+    @application1 = Application.create!(name: 'John Doe', address: '123 Elm St', description: 'Looking for a friendly dog', status: 'Pending')
     PetApplication.create!(pet: @pet1, application: @application1)
   end
+
   describe 'as a visitor' do
     describe 'when I visit an applications show page' do
       it 'then I can see their attributes' do
-        visit '/applications/1'
+        
+        visit "/applications/#{@application1.id}"
+        
         save_and_open_page
         expect(page).to have_content(@application1.name)
         expect(page).to have_content(@application1.address)
         expect(page).to have_content(@application1.description)
-        expect(page).to have_content(@application1.all_pets.name)
+        expect(page).to have_content(@application1.pets.name)
         expect(page).to have_content(@application1.status)
       end
     end
