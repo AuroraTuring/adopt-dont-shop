227458d6192fb42bea70a6a416e581a21bb2b9a8
Jan Lehigh <janlehigh461437@gmail.com>
2024-05-18 11:44:42 -0600

    Test new application form functionality.

--- spec/features/applications/new_spec.rb
+++ spec/features/applications/new_spec.rb
@@ -1,61 +1,46 @@
-	require 'rails_helper'
+require 'rails_helper'
 
-	RSpec.describe 'application new page' do
-		before(:each) do
-			@shelter1 = Shelter.create!(foster_program: true, name: 'Whimsy Whiskers', city: 'Denver', rank: 42)
-			# @pet1 = Pet.create!(name: 'Buddy', breed: 'Golden Retriever', age: 3, adoptable: true, shelter: @shelter1)
-			@pet1 = @shelter1.pets.create!(name: 'Buddy', breed: 'Golden Retriever', age: 3, adoptable: true)
-			@application1 = Application.create!(name: 'John Doe', address: '123 Elm St', description: 'Looking for a friendly dog', status: 'Pending')
-			PetApplication.create!(pet: @pet1, application: @application1)
-		end
+RSpec.describe 'application new page' do
+	describe 'as a visitor' do
+		describe 'when I visit the pet index page' do
+			it 'then I see a link to "Start an Application"' do
+				
+				visit "/pets"
 
-		describe 'as a visitor' do
-			describe 'when I visit the pet index page' do
-				it 'then I see a link to "Start an Application"' do
-					
-					visit "/pets"
-
-					expect(page).to have_link("Start an Application")
-				end
-
-				it 'when I click the link "Start an Applicaiton" I am taken to the new application page' do
-					
-					visit "/pets"
-
-					click_link "Start an Application"
-
-					expect(current_path).to eq("/applications/#{@application1.id}/new")
-				end
+				expect(page).to have_link("Start an Application")
 			end
 
-			describe 'when I visit the application new page' do
-				it "when I am taken to the new application page, I see a form" do
+			it 'when I click the link "Start an Applicaiton" I am taken to the new application page' do
+				
+				visit "/pets"
 
-					visit "/applications/#{@application1.id}/new"
+				click_link "Start an Application"
 
-					fill_in "application[name]", with: "Billy"
-					fill_in "application[address]", with: "1234 Example Street"
-					fill_in "application[city]", with: "Fake City"
-					fill_in "application[state]", with: "Faketucky"
-					fill_in "application[zip]", with: "123654"
-					fill_in "application[description]"
+				expect(current_path).to eq("/applications/new")
+			end
+		end
 
-				end
+		describe 'when I visit the application new page' do
+			it "when I am taken to the new application page, I see a form" do
 
-				it "" do
+				visit "/applications/new"
 
-				end
+				fill_in "name", with: "Billy"
+				fill_in "address", with: "1234 Example Street"
+				fill_in "city", with: "Fake City"
+				fill_in "state", with: "Faketucky"
+				fill_in "zip", with: "123654"
+				fill_in "description", with: "I would be a good parent because I would."
 
-				#   - Name
-				#   - Street Address
-				#   - City
-				#   - State
-				#   - Zip Code
-				#   - Description of why I would make a good home
-				# And I click submit
-				# Then I am taken to the new application's show page
-				# And I see my Name, address information, and description of why I would make a good home
-				# And I see an indicator that this application is "In Progress"
+				click_button "Submit Application" # within 'form' do find('#submit').click end
+			
+				expect(current_path).to eq("/applications/#{Application.last.id}")
+
+				expect(page).to have_content("Name: Billy")
+				expect(page).to have_content("Address: 1234 Example Street, Fake City, Faketucky 123654")
+				expect(page).to have_content("Description: I would be a good parent because I would.")
+
+				expect(page).to have_content("Application Status: In Progress")
 			end
 		end
 	end
