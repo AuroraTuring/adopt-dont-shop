88b930930df2e410ebd8c0e5ac3057b5dc5e81a3
Noah Durbin <13364668+noahdurbin@users.noreply.github.com>
2024-05-18 14:07:44 -0600

    test: can notice if a field is not filled out

--- spec/features/applications/new_spec.rb
+++ spec/features/applications/new_spec.rb
@@ -1,47 +1,60 @@
 require 'rails_helper'
 
 RSpec.describe 'application new page' do
-	describe 'as a visitor' do
-		describe 'when I visit the pet index page' do
-			it 'then I see a link to "Start an Application"' do
-				
-				visit "/pets"
+  describe 'as a visitor' do
+    describe 'when I visit the pet index page' do
+      it 'then I see a link to "Start an Application"' do
+        visit '/pets'
 
-				expect(page).to have_link("Start an Application")
-			end
+        expect(page).to have_link('Start an Application')
+      end
 
-			it 'when I click the link "Start an Applicaiton" I am taken to the new application page' do
-				
-				visit "/pets"
+      it 'when I click the link "Start an Applicaiton" I am taken to the new application page' do
+        visit '/pets'
 
-				click_link "Start an Application"
+        click_link 'Start an Application'
 
-				expect(current_path).to eq("/applications/new")
-			end
-		end
+        expect(current_path).to eq('/applications/new')
+      end
+    end
 
-		describe 'when I visit the application new page' do
-			it "when I am taken to the new application page, I see a form" do
+    describe 'when I visit the application new page' do
+      it 'when I am taken to the new application page, I see a form' do
+        visit '/applications/new'
 
-				visit "/applications/new"
+        fill_in 'name', with: 'Billy'
+        fill_in 'address', with: '1234 Example Street'
+        fill_in 'city', with: 'Fake City'
+        fill_in 'state', with: 'Faketucky'
+        fill_in 'zip', with: '123654'
+        fill_in 'description', with: 'I would be a good parent because I would.'
 
-				fill_in "name", with: "Billy"
-				fill_in "address", with: "1234 Example Street"
-				fill_in "city", with: "Fake City"
-				fill_in "state", with: "Faketucky"
-				fill_in "zip", with: "123654"
-				fill_in "description", with: "I would be a good parent because I would."
+        click_button 'Submit Application' # within 'form' do find('#submit').click end
 
-				click_button "Submit Application" # within 'form' do find('#submit').click end
-			
-				expect(current_path).to eq("/applications/#{Application.last.id}")
+        expect(current_path).to eq("/applications/#{Application.last.id}")
 
-				expect(page).to have_content("Name: Billy")
-				expect(page).to have_content("Address: 1234 Example Street, Fake City, Faketucky 123654")
-				expect(page).to have_content("Description: I would be a good parent because I would.")
+        expect(page).to have_content('Name: Billy')
+        expect(page).to have_content('Address: 1234 Example Street, Fake City, Faketucky 123654')
+        expect(page).to have_content('Description: I would be a good parent because I would.')
 
-				expect(page).to have_content("Application Status: In Progress")
-			end
-		end
-	end
+        expect(page).to have_content('Application Status: In Progress')
+      end
+    end
+
+    describe 'all fields in the new applications form must be filled out' do
+      it 'sends back to applications page and lets you know all fields are required' do
+        visit '/applications/new'
+
+        fill_in 'name', with: 'Billy'
+        fill_in 'address', with: '1234 Example Street'
+        fill_in 'city', with: 'Fake City'
+        fill_in 'state', with: 'Faketucky'
+        fill_in 'zip', with: '123654'
+
+        click_button 'Submit Application'
+
+        expect(page).to have_content("Description can't be blank")
+      end
+    end
+  end
 end
