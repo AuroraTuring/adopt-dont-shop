18f3ff9eed881740fbe9ca9fe61fb87e4f97af97
Noah Durbin <13364668+noahdurbin@users.noreply.github.com>
2024-05-19 15:30:16 -0600

    feat: added routes for applications show functionality

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   post '/applications', to: 'applications#create'
 
   get '/pet_applications/new', to: 'pet_applications#new'
-  post '/pet_applications', to: 'pet_applications#create'
+  post 'applications/:application_id/pet_applications', to: 'pet_applications#create'
 
   get '/shelters', to: 'shelters#index'
   get '/shelters/new', to: 'shelters#new'
