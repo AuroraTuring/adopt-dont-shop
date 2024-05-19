ff89af88b73167eef37b4f2ef5fcc832eb080664
Noah Durbin <13364668+noahdurbin@users.noreply.github.com>
2024-05-19 15:30:44 -0600

    feat: functionality for applications show functionality

--- app/controllers/pet_applications_controller.rb
+++ app/controllers/pet_applications_controller.rb
@@ -1,14 +1,11 @@
 class PetApplicationsController < ApplicationController
-  def create
-    binding.pry
-    @application = Application.find
-    binding.pry
-    @pet = Pet.find(params[:pet_name])
-    @pet_application = PetApplication.new({ id: params[:pet_application_id], application_id: params[:application_id],
-                                            pet_id: params[:pet_id] })
-    @pet_application.save
-    redirect_to "/#{@application}/show"
+  def new
+    @pet_application = PetApplication.new
   end
 
-  def new; end
+  def create
+    PetApplication.create(application_id: params[:application_id], pet_id: params[:pet_id])
+
+    redirect_to "/applications/#{params[:application_id]}"
+  end
 end
