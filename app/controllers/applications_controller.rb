2e45cb35f4f590a352757d82ca1b5b57d8b0745d
Noah Durbin <13364668+noahdurbin@users.noreply.github.com>
2024-05-19 17:11:11 -0600

    fix: changed show in application controller

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,9 +1,7 @@
 class ApplicationsController < ApplicationController
   def show
     @application = Application.find(params[:id])
-    return unless params[:pet_name]
-
-    @pets = Pet.search(params[:pet_name])
+    @pets = Pet.search(params[:pet_name]) if params[:pet_name]
   end
 
   def new
