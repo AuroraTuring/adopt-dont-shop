ddb365b14c34c044cba9173f006a86abdc57348b
Noah Durbin <13364668+noahdurbin@users.noreply.github.com>
2024-05-20 11:31:51 -0600

    test: model testing for applications and pet applications

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -1,14 +1,33 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe Application, type: :model do
-  describe "relationships" do
+  describe 'relationships' do
     it { should have_many(:pet_applications) }
-    it {should have_many(:pets, through: :pet_applications) }
+    it { should have_many(:pets, through: :pet_applications) }
   end
 
-  describe "validations" do
+  describe 'validations' do
     it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:adress) }
     it { should validate_presence_of(:city) }
-    it { should validate_presence_of(:rank) }
-    it { should validate_numericality_of(:rank) }
+    it { should validate_numericality_of(:state) }
+    it { should validate_numericality_of(:zip) }
+    it { should validate_numericality_of(:description) }
   end
+
+  before(:each) do
+    @shelter_1 = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+    @shelter_2 = Shelter.create(name: 'RGV animal shelter', city: 'Harlingen, TX', foster_program: false, rank: 5)
+
+    @pet_1 = @shelter_1.pets.create(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: false)
+    @pet_2 = @shelter_1.pets.create(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true)
+
+    @application1 = Application.create!(name: 'John Doe', address: '123 Elm St', city: 'Denver', state: 'CO',
+                                        zip: '12345', description: 'Looking for a friendly dog', status: 'In Progress')
+  end
+
+  describe 'class methods' do
+    it '' do
+    end
+  end
+end
--- spec/models/pet_application_spec.rb
+++ spec/models/pet_application_spec.rb
@@ -1,12 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe PetApplication, type: :model do
-  describe "relationships" do
+  describe 'relationships' do
     it { should have_many(:applications) }
     it { should have_many(:pets) }
   end
 
-  describe "validations" do
+  describe 'validations' do
     it { should validate_presence_of(:application_id) }
     it { should validate_presence_of(:pet_id) }
   end
+
+  before(:each) do
+    @shelter_1 = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+    @shelter_2 = Shelter.create(name: 'RGV animal shelter', city: 'Harlingen, TX', foster_program: false, rank: 5)
+
+    @pet_1 = @shelter_1.pets.create(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: false)
+    @pet_2 = @shelter_1.pets.create(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true)
+
+    @application1 = Application.create!(name: 'John Doe', address: '123 Elm St', city: 'Denver', state: 'CO',
+                                        zip: '12345', description: 'Looking for a friendly dog', status: 'In Progress')
+  end
+
+  describe 'class methods ' do
+    it '' do
+    end
+  end
+end
