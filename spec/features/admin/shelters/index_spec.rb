b9367fa51710cb89ed8488754a82274f5594e714
Aurora Sanderson <AuroraTuring@gmail.com>
2024-05-21 17:11:42 -0600

    Test for shelters listed in reverse alphabetical order

--- spec/features/admin/shelters/index_spec.rb
+++ spec/features/admin/shelters/index_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe 'admin shelter index' do
   before(:each) do
     @shelter1 = Shelter.create!(foster_program: true, name: "Whimsy Whiskers", city: "Denver", rank: 42)
-    @shelter2 = Shelter.create!(foster_program: false, name: "Sandy Meows", city: "Miami", rank: 42)
-    @shelter3 = Shelter.create!(foster_program: true, name: "Meow Wows", city: "New York", rank: 42)
+    @shelter2 = Shelter.create!(foster_program: true, name: "Meow Wows", city: "New York", rank: 42)
+    @shelter3 = Shelter.create!(foster_program: false, name: "Sandy Meows", city: "Miami", rank: 42)
   end
 
   # As a visitor
@@ -15,8 +15,17 @@ RSpec.describe 'admin shelter index' do
       it 'then I see all Shelters in the system listed in reverse alphabetical order by name' do
         visit "/admin/shelters"
 
-        expect(page).to have_content("Whimsy Whiskers")
-        # order(name: :desc)
+        within "#shelter-0" do
+          expect(page).to have_content("Whimsy Whiskers")
+        end
+
+        within "#shelter-1" do
+        expect(page).to have_content("Sandy Meows")
+        end
+
+        within "#shelter-2" do
+        expect(page).to have_content("Meow Wows")
+        end
       end
     end
   end
