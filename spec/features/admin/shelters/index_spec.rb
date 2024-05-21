be73952c8316960e7927ba0fb7a4059707dec823
Aurora Sanderson <AuroraTuring@gmail.com>
2024-05-20 20:23:24 -0600

    Begin testing user story ten

---
+++ spec/features/admin/shelters/index_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe 'admin shelter index' do
+  before(:each) do
+    @shelter1 = Shelter.create!(foster_program: true, name: "Whimsy Whiskers", city: "Denver", rank: 42)
+    @shelter2 = Shelter.create!(foster_program: false, name: "Sandy Meows", city: "Miami", rank: 42)
+    @shelter3 = Shelter.create!(foster_program: true, name: "Meow Wows", city: "New York", rank: 42)
+  end
+
+  # As a visitor
+  # When I visit the admin shelter index ('/admin/shelters')
+  # Then I see all Shelters in the system listed in reverse alphabetical order by name
+  describe 'as a visitor' do
+    describe 'when I visit the admin shelter index ("/admin/shelters")' do
+      it 'then I see all Shelters in the system listed in reverse alphabetical order by name' do
+        visit "/admin/shelters"
+
+        expect(page).to have_content("Whimsy Whiskers")
+        # order(name: :desc)
+      end
+    end
+  end
+
+end
