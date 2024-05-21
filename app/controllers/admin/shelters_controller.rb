1b23142a8c94f9a897dd91ca182df2da80860ef0
Aurora Sanderson <AuroraTuring@gmail.com>
2024-05-21 16:52:24 -0600

    Displays shelters within index in reverse alphabetical order

--- app/controllers/admin/shelters_controller.rb
+++ app/controllers/admin/shelters_controller.rb
@@ -1,5 +1,5 @@
 class Admin::SheltersController < ApplicationController
   def index
-    @shelters = Shelter.find_by_sql("SELECT * FROM shelters ORDER BY shelters.name desc")
+    @shelters = Shelter.reverse_alphabetical_shelters
   end
 end
