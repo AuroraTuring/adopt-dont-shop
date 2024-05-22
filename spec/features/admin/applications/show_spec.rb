b7f4fe2d9a295590f676d67de761c7bfe79df80c
Noah Durbin <13364668+noahdurbin@users.noreply.github.com>
2024-05-21 20:13:51 -0600

    test: test for the button to reject an application

---
+++ spec/features/admin/applications/show_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe 'application show' do
+  before do
+    @shelter1 = Shelter.create!(foster_program: true, name: 'Whimsy Whiskers', city: 'Denver', rank: 42)
+    @pet1 = @shelter1.pets.create!(name: 'Buddy', breed: 'Golden Retriever', age: 3, adoptable: true)
+    @application1 = Application.create!(name: 'John Doe', address: '123 Elm St', city: 'Denver', state: 'CO',
+                                        zip: '12345', description: 'Looking for a friendly dog', status: 'In Progress')
+  end
+
+  describe 'as a visitor' do
+    it 'there is a button to reject an application' do
+      visit "/admin/applications/#{@application1}"
+
+      expect(page).to have_content('Reject Application')
+    end
+  end
+end
+# As a visitor
+# When I visit an admin application show page ('/admin/applications/:id')
+# For every pet that the application is for, I see a button to reject the application for that specific pet
+# When I click that button
+# Then I'm taken back to the admin application show page
+# And next to the pet that I rejected, I do not see a button to approve or reject this pet
+# And instead I see an indicator next to the pet that they have been rejected
