a1c6ef5b2599f3faa654a601a380ca0a7a993764
Noah Durbin <13364668+noahdurbin@users.noreply.github.com>
2024-05-22 12:13:28 -0600

    feat: rejection and approval button, tests for approval button

--- app/controllers/admin/applications_controller.rb
+++ app/controllers/admin/applications_controller.rb
@@ -1,12 +1,13 @@
 class Admin::ApplicationsController < ApplicationController
   def show
     @application = Application.find(params[:id])
+    @pet_applications = PetApplication.by_application_id(@application.id)
     @pets = Pet.search(params[:pet_name]) if params[:pet_name]
   end
 
   def update
     @application = Application.find(params[:id])
-    @pet_application = PetApplication.find(params[:pet_application_id])
+    @pet_application = @application.pet_applications.find_by(params[:pet_id])
     if @pet_application.update(pet_application_params)
       redirect_to "/admin/applications/#{@application.id}"
     else
--- spec/features/admin/applications/show_spec.rb
+++ spec/features/admin/applications/show_spec.rb
@@ -6,30 +6,41 @@ RSpec.describe 'application show' do
     @pet1 = @shelter1.pets.create!(name: 'Buddy', breed: 'Golden Retriever', age: 3, adoptable: true)
     @application1 = Application.create!(name: 'John Doe', address: '123 Elm St', city: 'Denver', state: 'CO',
                                         zip: '12345', description: 'Looking for a friendly dog', status: 'In Progress')
-    PetApplication.create!(pet: @pet1, application: @application1, status: 'Pending')
+    PetApplication.create!(pet: @pet1, application: @application1, status: :Pending)
   end
 
   describe 'as a visitor' do
     it 'there is a button to reject an application' do
       visit "/admin/applications/#{@application1.id}"
 
-      expect(page).to have_content("Reject #{@pet1.name}")
+      expect(page).to have_content("Reject")
     end
 
     it 'when a pet is rejected from an application there is a status of rejected and button to reject is gone' do
       visit "/admin/applications/#{@application1.id}"
+      
       expect(page).to_not have_content('Rejected')
-
       click_button 'Reject'
-      save_and_open_page
+      
       expect(page).to have_content('Rejected')
+    end
+
+    it 'there is a button to approve an application' do
+      visit "/admin/applications/#{@application1.id}"
+
+      expect(page).to have_content("Approve")
+    end
+
+    it 'when a pet is Approved on an application there is a status of Approved and button to Approve is gone' do
+      visit "/admin/applications/#{@application1.id}"
+      
+      expect(page).to_not have_content('Approved')
+      click_button 'Approve'
+      
+      expect(page).to have_content('Approved')
     end
   end
 end
-# As a visitor
-# When I visit an admin application show page ('/admin/applications/:id')
-# For every pet that the application is for, I see a button to reject the application for that specific pet
-# When I click that button
 # Then I'm taken back to the admin application show page
 # And next to the pet that I rejected, I do not see a button to approve or reject this pet
 # And instead I see an indicator next to the pet that they have been rejected
