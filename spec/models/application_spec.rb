27097245b6100407a822e45efd68214a13adcdcd
Jan Lehigh <janlehigh461437@gmail.com>
2024-05-22 18:01:09 -0600

    Refactored adress to address in validation tests

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Application, type: :model do
 
   describe 'validations' do
     it { should validate_presence_of(:name) }
-    it { should validate_presence_of(:adress) }
+    it { should validate_presence_of(:address) }
     it { should validate_presence_of(:city) }
     it { should validate_numericality_of(:state) }
     it { should validate_numericality_of(:zip) }
